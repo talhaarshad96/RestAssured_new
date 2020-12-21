128f10de69e3abe4d22a869ca62bb1f480ca36d1
Admin <Admin@Talha.lan>
2020-12-21 09:17:37 +0500

    After.

--- RestAssured/src/test/java/Test_GET.java
+++ RestAssured/src/test/java/Test_GET.java
@@ -1,14 +1,24 @@
+import org.json.simple.JSONObject;
 import org.testng.Assert;  //M. Talha Arshad
 import org.testng.annotations.Test;
+
+
+
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.*; //static because now, directly use
-											//methods without creating objects.
+
+import io.restassured.http.ContentType;
+//methods without creating objects.
 import io.restassured.response.Response;
 
 public class Test_GET 
 {
-	@Test 
+	//@Test 
 	void test_01() 
 	{
 		//Response resp = RestAssured.get("https://reqres.in/api/users?page=2"); //example is this, cant use RestAssured.get()
@@ -27,11 +37,89 @@ public class Test_GET
 		Assert.assertEquals(statusCode, 200);
 	}
 	
-	@Test
+	//@Test
 	void test_02()
 	{
 		given().get("https://reqres.in/api/users?page=2")
 		.then().statusCode(200)
 		.body("data.id[0]", equalTo(7));
+	}
+	
+	//@Test
+	void test_03()
+	{
+		given().get("https://reqres.in/api/users?page=2")
+		.then().statusCode(200)
+		.body("data.id[0]", equalTo(7))
+		.body("data.first_name", hasItems("Michael"));
+		//.log().all();
+	}
+	
+	//@Test
+	void test_04()
+	{
+		//Map<String, Object> map =  new HashMap <String, Object>();
+		
+		//map.put("name","talha");
+		//map.put("bla","blabber");
+		
+		//System.out.println(map);
+	
+		JSONObject request = new JSONObject();
+		
+		request.put("name", "talha");
+		request.put("job", "blabber");
+		//System.out.println(request);
+		System.out.println("1 "+request);
+		
+		given().body(request.toJSONString()).
+		contentType (ContentType.JSON).
+		when().
+		post("https://reqres.in/api/users").
+		then().
+		statusCode(201)
+		.log().all();
+		
+	}
+	
+	@Test(priority=1)
+	void test_05() throws InterruptedException
+	{
+		JSONObject request = new JSONObject();
+		
+		request.put("name","talha");
+		request.put("job","blabber");
+
+		System.out.println("1 "+request);
+		
+		given().body(request.toJSONString()).
+		contentType (ContentType.JSON).
+		when().
+		put("https://reqres.in/api/users/2").
+		then().
+		statusCode(200)
+		.log().all();
+		System.out.println("test05");
+		//Thread.sleep(-10);
+	}
+	
+	@Test(priority=0)
+	void test_06()
+	{
+		JSONObject request = new JSONObject();
+		
+		request.put("name","talha");
+		request.put("job","blabber");
+
+		System.out.println("1 "+request);
+		
+		given().body(request.toJSONString()).
+		contentType (ContentType.JSON).
+		when().
+		patch("https://reqres.in/api/users/2").
+		then().
+		statusCode(200)
+		.log().all();
+		System.out.println("test06");
 	}
 }
